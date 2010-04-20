a60b0d60420c19a74df2b6f068e235a8b853487a
niklas.lind <niklas.lind@valtech.se>
2010-04-20 18:58:19 +0200

    updated setup keys

--- lab1/step_1_setup/setenv.sh
+++ lab1/step_1_setup/setenv.sh
@@ -1,9 +1,9 @@
 
 
-export EC2_LAB_HOME=~/worklocal/valtechlab/EC2_lab/lab1
-export EC2_HOME=~/.e2c/ec2-api-tools-1.3-46266/
+export EC2_LAB_HOME=/tmp/lab/EC2_lab
+export EC2_HOME=~/.ec2-tmp/ec2-api-tools-1.3-46266/
 
-export PATH=$PATH:$EC2_HOME/bin
+export PATH=$EC2_HOME/bin:$PATH
 export EC2_PRIVATE_KEY=$EC2_LAB_HOME/keys/pk-B2NCLKAPSNPIIQC2ADSPWXWVB33G73LJ.pem
 export EC2_CERT=$EC2_LAB_HOME/keys/cert-B2NCLKAPSNPIIQC2ADSPWXWVB33G73LJ.pem
 
